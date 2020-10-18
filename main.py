35ce0475349cd90812a0d60f1173a741fcf666f9
Sayeed Md. Shaiban <evrydaywannabe@pop-os.localdomain>
2020-10-18 20:14:54 +0600

    added game screen

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import pygame
+
+pygame.init()
+
+# create a display screen
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Space game")
+
+game_loop = True
+
+while game_loop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_loop = False
+
+    screen.fill((255,255,255))
+    pygame.display.update()
